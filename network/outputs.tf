440e3e741c50bb6da7891020c60de499e1dffba6
Ben Mechen <ben.mechen@3sidedcube.com>
2023-03-20 17:12:47 +0000

    fix(network): Remove unused output

--- network/outputs.tf
+++ network/outputs.tf
@@ -6,10 +6,10 @@ output "cidr" {
   value = module.vpc.vpc_cidr_block
 }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnets
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
 }
 
-output "lambda_security_group_id" {
-  value = aws_security_group.lambda.id
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
 }
