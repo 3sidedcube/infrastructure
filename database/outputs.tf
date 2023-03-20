f8f316da8e903894c86359d4f2ca4bdb47e600c4
Ben Mechen <ben.mechen@3sidedcube.com>
2023-03-20 17:16:22 +0000

    fix(database): Add missing outputs

--- database/outputs.tf
+++ database/outputs.tf
@@ -1,5 +1,21 @@
 output "password" {
   description = "Database password"
-  value = random_password.main.result
-  sensitive = true
+  value       = random_password.main.result
+  sensitive   = true
+}
+
+output "endpoint" {
+  value = aws_rds_cluster.main.endpoint
+}
+
+output "port" {
+  value = aws_rds_cluster.main.port
+}
+
+output "database_name" {
+  value = aws_rds_cluster.main.database_name
+}
+
+output "security_group_id" {
+  value = aws_security_group.main.id
 }
