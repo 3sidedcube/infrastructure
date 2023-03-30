3ab424b9f29f491b4b128d12702bc69315b9ce4b
Ben Mechen <ben.mechen@3sidedcube.com>
2023-03-30 17:31:05 +0100

    infra(domain): Add zone name to output

--- domain/outputs.tf
+++ domain/outputs.tf
@@ -3,6 +3,11 @@ output "zone_id" {
   value       = aws_route53_zone.main.zone_id
 }
 
+output "zone_name" {
+  description = "Root name of created Zone"
+  value       = aws_route53_zone.main.name
+}
+
 output "nameservers" {
   description = "Nameservers"
   value       = aws_route53_zone.main.name_servers
