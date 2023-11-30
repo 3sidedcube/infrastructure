b7f138ef16abefcedd9c8ca2bba4bcc2556821e6
Olatokumbo <faithodesola@gmail.com>
2023-11-30 16:49:38 +0000

    Add rules to WAF

--- firewall/main.tf
+++ firewall/main.tf
@@ -11,4 +11,49 @@ resource "aws_wafv2_web_acl" "main" {
     metric_name                = "${var.project_name}-WAF"
     sampled_requests_enabled   = true
   }
+
+  rule {
+    name     = "AWSManagedRulesSQLiRuleSet"
+    priority = 50
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesSQLiRuleSetMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesAdminProtectionRuleSet"
+    priority = 52
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAdminProtectionRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAdminProtectionRuleSetMetric"
+      sampled_requests_enabled   = true
+    }
+  }
 }
+
